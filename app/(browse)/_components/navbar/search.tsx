befd3842d2abec34c3296a239b8f621625061e92
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-02-15 15:00:54 +0100

    Header Search component done

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -9,17 +9,48 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 const Search = () => {
+    const router = useRouter();
+    const [value, setValue] = useState("");
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (!value) return;
+
+        const url = qs.stringifyUrl(
+            {
+                url: "/search",
+                query: { term: value },
+            },
+            { skipEmptyString: true }
+        );
+
+        router.push(url);
+    };
+    const onClear = () => {
+        setValue('')
+    }
+
     return (
         <form
-            onSubmit={() => {}}
+            onSubmit={onSubmit}
             className="relative w-full lg:w-[400px] flex items-center"
         >
-            <Input placeholder="Search" />
+            <Input
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                placeholder="Search"
+                className="rounde-r-none focus-visible:ring-0 focus-visible:ring-transperent focus-visible:ring-offset-0"
+            />
+            {value && (
+                <X className="absolute top-2.5 right-14 h-5 w-5 text-muted-foreground cursor-pointer hover:opacity-75 transition" 
+                    onClick={onClear}
+                />
+            )}
             <Button
                 type="submit"
                 size={"sm"}
                 variant={"secondary"}
-                className="rounded-none"
+                className="rounded-l-none"
             >
                 <SearchIcon className="w-5 h-5 text-muted-foreground" />
             </Button>
