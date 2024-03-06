341cfba1dc11cc2a98715454c0f3ec48d016adda
Danylo Ohurtsov <d.ohurtsov.fed@gmail.com>
2024-03-06 11:42:01 +0100

    Enhancement [Feature] Created a new component for toggling chat settings on the stream. [Feature] Added a new server action to enable updating stream settings.
    UAH:
    Покращення
    [Оновлення] Створено новий компонент для перемикання налаштувань чату на стрімі.
    [Оновлення] Додано нову серверну дію для можливості оновлення налаштувань стріму.

--- app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
+++ app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Switch } from "@/components/ui/switch";
+
 type FieldTypes = "isChatEnabled" | "isChatDelayed" | "isChatFollowersOnly";
 
 interface ToggleCardProps {
@@ -8,8 +10,17 @@ interface ToggleCardProps {
     value: boolean;
 }
 
-const ToggleCard = ({field, value, label}: ToggleCardProps) => {
-    return <div>ToggleCard</div>;
+const ToggleCard = ({ field, value = false, label }: ToggleCardProps) => {
+    return (
+        <div className=" rounded-xl bg-muted p-6">
+            <div className=" flex items-center justify-between">
+                <p className=" font-semibold shrink-0">{label}</p>
+                <div className="space-y-2">
+                    <Switch checked={value}>{value ? "On" : "Off"}</Switch>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ToggleCard;
