3f5ab87a84ecf243eaa94e436c58936a9e97a473
Danylo Ohurtsov <d.ohurtsov.fed@gmail.com>
2024-03-06 12:15:59 +0100

    Replaced Static Elements with Mapped Function
    Refactored code by replacing three static elements with an array mapped function.
    This optimization enhances code readability and scalability.

--- app/(dashboard)/u/[username]/chat/loading.tsx
+++ app/(dashboard)/u/[username]/chat/loading.tsx
@@ -6,9 +6,9 @@ const ChatLoading = () => {
         <div className=" p-6 space-y-4">
             <Skeleton className=" h-10 w-[200px]" />
             <div className="space-y-4">
-                <ToggleCardSkeleton />
-                <ToggleCardSkeleton />
-                <ToggleCardSkeleton />
+                {[...Array(3)].map((_, index) => (
+                    <ToggleCardSkeleton key={index} />
+                ))}
             </div>
         </div>
     );
