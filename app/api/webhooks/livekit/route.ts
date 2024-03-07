f2eaba604d923335da373ceba97f44d207d001fa
Danylo Ohurtsov <d.ohurtsov.fed@gmail.com>
2024-03-07 23:15:34 +0100

    minor changes

--- app/api/webhooks/livekit/route.ts
+++ app/api/webhooks/livekit/route.ts
@@ -10,7 +10,7 @@ const receiver = new WebhookReceiver(
 
 export async function POST(req: Request) {
     const body = await req.text();
-  
+
     const headerPayload = headers();
     const authorization = headerPayload.get("Authorization");
 
@@ -19,7 +19,7 @@ export async function POST(req: Request) {
     }
 
     const event = receiver.receive(body, authorization);
-  
+
     if (event.event === "ingress_started") {
         await db.stream.update({
             where: {
