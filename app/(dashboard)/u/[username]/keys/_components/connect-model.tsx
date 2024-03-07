d9f88800795b6d754ac97ad64f9030fb13502d95
Danylo Ohurtsov <d.ohurtsov.fed@gmail.com>
2024-03-07 18:39:26 +0100

    Ingress Functions Integrated in Component
    Implemented Ingress creation and reset functions within component.
    Connected to 'Generate Ingress' button.

--- app/(dashboard)/u/[username]/keys/_components/connect-model.tsx
+++ app/(dashboard)/u/[username]/keys/_components/connect-model.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import { toast } from "sonner";
+import { useState, useTransition, useRef, ElementRef } from "react";
 import { AlertTriangle } from "lucide-react";
+import { IngressInput } from "livekit-server-sdk";
+
+import { createIngress } from "@/actions/ingress";
 import { Button } from "@/components/ui/button";
 import {
     Dialog,
     DialogClose,
+    DialogContent,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
-    DialogContent,
 } from "@/components/ui/dialog";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import {
@@ -19,28 +24,51 @@ import {
     SelectValue,
 } from "@/components/ui/select";
 
-const ConnectModel = () => {
+const RTMP = String(IngressInput.RTMP_INPUT);
+const WHIP = String(IngressInput.WHIP_INPUT);
+
+type IngressType = typeof RTMP | typeof WHIP;
+
+const ConnectModal = () => {
+    const closeRef = useRef<ElementRef<"button">>(null);
+    const [isPending, startTransition] = useTransition();
+    const [ingressType, setIngressType] = useState<IngressType>(RTMP);
+
+    const onSubmit = () => {
+        startTransition(() => {
+            createIngress(parseInt(ingressType))
+                .then(() => {
+                    toast.success("Ingress created");
+                    closeRef?.current?.click();
+                })
+                .catch(() => toast.error("Something went wrong"));
+        });
+    };
+
     return (
         <Dialog>
             <DialogTrigger asChild>
-                <Button variant={"primary"}>Generate Connection</Button>
+                <Button variant="primary">Generate connection</Button>
             </DialogTrigger>
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle>Generate connection</DialogTitle>
                 </DialogHeader>
-                <Select>
+                <Select
+                    disabled={isPending}
+                    value={ingressType}
+                    onValueChange={(value) => setIngressType(value)}
+                >
                     <SelectTrigger className="w-full">
                         <SelectValue placeholder="Ingress Type" />
                     </SelectTrigger>
                     <SelectContent>
-                        <SelectItem value="RTMP">RTMP</SelectItem>
-                        <SelectItem value="WHIP">WHIP</SelectItem>
-
+                        <SelectItem value={RTMP}>RTMP</SelectItem>
+                        <SelectItem value={WHIP}>WHIP</SelectItem>
                     </SelectContent>
                 </Select>
                 <Alert>
-                    <AlertTriangle className="h-5 w-5" />
+                    <AlertTriangle className="h-4 w-4" />
                     <AlertTitle>Warning!</AlertTitle>
                     <AlertDescription>
                         This action will reset all active streams using the
@@ -48,10 +76,14 @@ const ConnectModel = () => {
                     </AlertDescription>
                 </Alert>
                 <div className="flex justify-between">
-                    <DialogClose>
-                        <Button variant={"ghost"}>Cancel</Button>
+                    <DialogClose ref={closeRef} asChild>
+                        <Button variant="ghost">Cancel</Button>
                     </DialogClose>
-                    <Button variant={"primary"} onClick={() => {}}>
+                    <Button
+                        disabled={isPending}
+                        onClick={onSubmit}
+                        variant="primary"
+                    >
                         Generate
                     </Button>
                 </div>
@@ -59,5 +91,4 @@ const ConnectModel = () => {
         </Dialog>
     );
 };
-
-export default ConnectModel;
+export default ConnectModal;
