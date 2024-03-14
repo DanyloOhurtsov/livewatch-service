daf8791901bc0285574db9d2b81b9d4c0be228eb
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-03-14 20:49:14 +0100

    Progress commit

--- components/stream-player/chat-header.tsx
+++ components/stream-player/chat-header.tsx
@@ -1,9 +1,23 @@
-'use client'
+"use client";
+
+import { Skeleton } from "../ui/skeleton";
 
 const ChatHeader = () => {
-  return (
-    <div>ChatHeader</div>
-  )
-}
+    return (
+        <div className=" relative p-3 border-b">
+            <p className=" font-semibold text-primary text-center">
+                Stream chat
+            </p>
+        </div>
+    );
+};
 
-export default ChatHeader
+export default ChatHeader;
+
+export const ChatHeaderSkeleton =() => {
+    return(
+        <div className=" relative p-3 border-b hidden">
+
+        </div>
+    )
+}
