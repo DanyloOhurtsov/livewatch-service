28b631b6fdf86157475a0fa140c048f9d5f3c6de
Danylo Ohurtsov <d.ohurtsov.fed@gmail.com>
2024-03-22 12:54:47 +0100

    Thumbnail component ini

--- app/(browse)/(home)/_components/result-card.tsx
+++ app/(browse)/(home)/_components/result-card.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Stream, User } from "@prisma/client";
+import { Thumbnail } from "@/components/components";
 
 interface ResultCardProps {
     data: Stream & { user: User };
@@ -9,12 +10,12 @@ const ResultCard = ({ data }: ResultCardProps) => {
     return (
         <Link href={`/${data.user.username}`}>
             <div className=" h-full w-full space-y-4">
-                {/* <Thumbnail
+                <Thumbnail
                     src={data.thumbnailUrl}
                     fallback={data.user.imageUrl}
                     isLive={data.isLive}
                     username={data.user.username}
-                /> */}
+                />
             </div>
         </Link>
     );
--- components/components.ts
+++ components/components.ts
@@ -1,6 +1,14 @@
 import { HintTooltip } from "./hint";
 import LiveBadge from "./live-badge";
+import Thumbnail from "./thumbnail";
 import UserAvatar, { UserAvatarSkeleton } from "./user-avatar";
 import VerifiedMark from "./verified-mark";
 
-export { HintTooltip, LiveBadge, UserAvatar, UserAvatarSkeleton, VerifiedMark };
+export {
+    HintTooltip,
+    LiveBadge,
+    UserAvatar,
+    UserAvatarSkeleton,
+    VerifiedMark,
+    Thumbnail,
+};
