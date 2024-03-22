f7422a70d0ea44fcb0b09bfc0b79526ac836e02a
Danylo Ohurtsov <d.ohurtsov.fed@gmail.com>
2024-03-22 12:51:31 +0100

    Home page init

---
+++ app/(browse)/(home)/_components/components.ts
@@ -0,0 +1,4 @@
+import Results, { ResultsSkeleton } from "./results";
+import ResultCard from "./result-card";
+
+export { Results, ResultsSkeleton, ResultCard };
---
+++ app/(browse)/(home)/_components/result-card.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { Stream, User } from "@prisma/client";
+
+interface ResultCardProps {
+    data: Stream & { user: User };
+}
+
+const ResultCard = ({ data }: ResultCardProps) => {
+    return (
+        <Link href={`/${data.user.username}`}>
+            <div className=" h-full w-full space-y-4">
+                {/* <Thumbnail
+                    src={data.thumbnailUrl}
+                    fallback={data.user.imageUrl}
+                    isLive={data.isLive}
+                    username={data.user.username}
+                /> */}
+            </div>
+        </Link>
+    );
+};
+
+export default ResultCard;
